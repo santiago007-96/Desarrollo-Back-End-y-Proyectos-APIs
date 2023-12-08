522c7cb50c33b2f625fa05be6b741ff145268c9a
santiago007-96 <sacamacho@espe.edu.ec>
2023-12-07 20:46:41 -0500

    Botón modificar imagen 2

--- Ejercicio Git/ejercicio.js
+++ Ejercicio Git/ejercicio.js
@@ -1,5 +1,6 @@
 saludar = function() {
     let cmpImagenSaludo;
     cmpImagenSaludo = document.getElementById("imgSaludo");
+    cmpImagenSaludo.src = "./imagenes/yagane-gane.gif";
     console.log("Ingresa a saludar");
 }
