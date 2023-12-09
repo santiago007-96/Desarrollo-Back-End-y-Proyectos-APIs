93621944180e9c189469e62f2ca7c5154d7b6665
santiago007-96 <sacamacho@espe.edu.ec>
2023-12-09 13:28:11 -0500

    Composicion

---
+++ Composicion/src/com/krakedev/composicion/Persona.java
@@ -0,0 +1,43 @@
+package com.krakedev.composicion;
+
+public class Persona {
+	private String nombre;
+	private String apellido;
+	private int edad;
+	private double estatura;
+	private Direccion direccion;
+	
+	
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public double getEstatura() {
+		return estatura;
+	}
+	public void setEstatura(double estatura) {
+		this.estatura = estatura;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public Direccion getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
+	}
+	
+	
+}
---
+++ Composicion/src/com/krakedev/test/TestPersona.java
@@ -0,0 +1,16 @@
+package com.krakedev.test;
+
+import com.krakedev.composicion.Direccion;
+import com.krakedev.composicion.Persona;
+
+public class TestPersona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona p1 = new Persona();
+		String nombre = p1.getNombre();
+		Direccion d1 = p1.getDireccion();
+		
+	}
+
+}
